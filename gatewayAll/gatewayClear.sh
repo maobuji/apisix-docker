2c31a9090f3b89d1342c9379c918ef21496859bd
maobuji <16770864@qq.com>
2020-08-07 12:47:55 +0800

    更新版本

--- gatewayAll/gatewayClear.sh
+++ gatewayAll/gatewayClear.sh
@@ -2,7 +2,7 @@
 docker-compose -p docker-apisix down
 rm apisix_log/* -rf
 rm etcd_conf/* -rf
-docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix:1.4.1
+docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix:latest
 docker rmi bitnami/etcd:3.4.9
 docker rmi ruby:2-alpine
 docker images
