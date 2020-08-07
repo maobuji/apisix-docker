def15a82bb6ca5c53f729a02ca59425c768f52b8
maobuji <16770864@qq.com>
2020-08-07 16:55:08 +0800

    通过镜像引入管理端

--- gatewayAll/gatewayClear.sh
+++ gatewayAll/gatewayClear.sh
@@ -3,6 +3,8 @@ docker-compose -p docker-apisix down
 rm apisix_log/* -rf
 rm etcd_conf/* -rf
 docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix:latest
+docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix-dashboard:latest
+docker rmi registry.cn-shenzhen.aliyuncs.com/maobuji/apisix-manager:latest
 docker rmi bitnami/etcd:3.4.9
 docker rmi ruby:2-alpine
-docker imagesore
+docker images
