0b07684888c4c0d732cb28a1a06f3e546bad223c
maobuji <16770864@qq.com>
2020-08-10 08:28:38 +0800

    增加帮助命令

--- gatewayAll/gatewayHelp.sh
+++ gatewayAll/gatewayHelp.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-echo "docker rmi $image"
-echo "docker inspect $Container"
-echo "docker logs $Container"
+echo "docker rmi $imageID 删除镜像"
+echo "docker inspect $ContainerID 查看容器详细信息"
+echo "docker logs $ContainerID 查看容器详细信息"
+echo "docker network ls 查看docker网络信息"
+echo "docker network rm $networkName  删除网络配置"
+
